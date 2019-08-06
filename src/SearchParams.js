9ca42b6b0f41dd68cf9ff0c5df426ce0306f0173
ronan-f <rfegan13@gmail.com>
2019-08-05 18:25:58 -0700

    Adding package and using it to create a select element

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,20 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlue={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => {
+              return <option value={animal}>{animal}</option>;
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
