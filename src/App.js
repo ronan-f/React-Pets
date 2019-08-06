afdcae98bc5d77f4eeb71900e1dc47c82224ffec
ronan-f <rfegan13@gmail.com>
2019-08-05 18:00:49 -0700

    Creating search component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Rue" animal="Dog" breed="Poodle" />
-      <Pet name="Tom" animal="Cat" breed="Stray" />
-      <Pet name="Jerry" animal="Mouse" breed="Field" />
+      <SearchParams />
     </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
