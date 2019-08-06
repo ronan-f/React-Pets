bbf19427fa4ff0c1fcd264c6ebc26dc0205766a7
ronan-f <rfegan13@gmail.com>
2019-08-05 18:10:41 -0700

    Adding useState to keep track of form input

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
