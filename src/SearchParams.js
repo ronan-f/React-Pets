b030c44cff8c5b4b5834d2e27bc4c829bf13f096
ronan-f <rfegan13@gmail.com>
2019-08-07 21:04:02 -0700

    Adding useEffect hook to call API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import UseDropdown from "./UseDropdown";
 import useDropdown from "./UseDropdown";
 
@@ -8,6 +8,10 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    pet.breeds("dog").then(console.log, console.error);
+  });
 
   return (
     <div className="search-params">
